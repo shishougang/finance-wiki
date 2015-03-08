04a0b87c72d9454c4fc4ef3f620a22c355f141c4
SHI Shougang <shishougang@gmail.com>
2015-03-08 18:20:01 +0800

    update

--- publish-wiki.sh
+++ publish-wiki.sh
@@ -2,6 +2,10 @@
 
 #TODO:  syntax hight fails
 #emacs24 --batch -l ./src/publish_config.el -f publish-org
+if [ $1 = "preview" ]; then 
+    python -m SimpleHTTPServer 9999
+    return
+fi
 
 git add .
 if [ $# -eq 1 ]; then
